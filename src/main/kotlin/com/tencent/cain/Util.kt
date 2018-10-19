43afbf0e18b78940c3d2e9a461dc73c619123275
cainjiang <cainjiang@tencent.com>
2018-10-19 13:05:28 +0800

    顶级函数与属性

---
+++ src/main/kotlin/com/tencent/cain/Util.kt
@@ -0,0 +1,13 @@
+@file:JvmName("Utils")
+package com.tencent.cain
+
+@JvmOverloads
+fun <T> joinToString(collection: Collection<T>, separator: String = ", ", prefix: String = "", postfix: String = ""): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
