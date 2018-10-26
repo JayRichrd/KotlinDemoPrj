6364f9242332647c7d1756c2a2fb0fdf7d05a3bb
cainjiang <cainjiang@tencent.com>
2018-10-27 01:30:53 +0800

    java实现kotlin中的接口，必须实现所有的方法

---
+++ src/main/java/com/tencent/cain/java/JavaButton.java
@@ -0,0 +1,26 @@
+package com.tencent.cain.java;
+
+import com.tencent.cain.itf.IClickable;
+import com.tencent.cain.itf.IFocusable;
+
+public class JavaButton implements IClickable,IFocusable {
+    @Override
+    public void click() {
+
+    }
+
+    @Override
+    public void showOff() {
+
+    }
+
+    @Override
+    public void hello() {
+
+    }
+
+    @Override
+    public void setFocus(boolean b) {
+
+    }
+}
